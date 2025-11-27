0a72b9ad42d22763b4e1392a166c56887d2dae37
Loukan Elkadi <loukan.el.kadi@gmail.com>
2025-11-27 13:47:47 +0200

    feat: Encapsulate resource data and deletion logic within ResourcesList by moving state from App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <TheHeader title="RememberMe" />
-  <ResourcesList :resources="storedResources" @delete="removeResource" />
-  <TheFooter />
+    <ResourcesList />
+    <TheFooter />
 </template>
 
 <script>
@@ -16,21 +16,6 @@ export default {
     TheHeader,
     TheFooter
   },
-  data() {
-    return {
-      storedResources: [
-        { id: 1, title: 'Vue.js', url: 'https://vuejs.org', description: 'The progressive JavaScript framework.' },
-        { id: 2, title: 'Vue Router', url: 'https://router.vuejs.org', description: 'The official router for Vue.js.' },
-        { id: 3, title: 'Vue Composition API', url: 'https://composition-api.vuejs.org', description: 'API for composing logic in Vue components.' },
-        { id: 4, title: 'Vue Test Utils', url: 'https://test-utils.vuejs.org', description: 'Official testing utilities for Vue.js.' },
-      ]
-    }
-  },
-  methods: {
-    removeResource(resourceId) {
-      this.storedResources = this.storedResources.filter(resource => resource.id !== resourceId);
-    }
-  }
 }
 </script>
 
